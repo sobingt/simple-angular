2da3a78a1cd622df6048934e64c03daef9eb6de3
Sobin George Thomas <sobingt@gmail.com>
2015-12-04 16:09:20 +0530

    examples of angular module

--- app.js
+++ app.js
@@ -6,18 +6,13 @@
         templateUrl: 'list.html',
         controller: 'Ctrl'
       })
-      .when('/edit',{
+      .when('/edit/:id',{
         templateUrl: 'edit.html',
         controller: 'Edit'
       })
     })
-    .controller('Ctrl',['$scope', function(scope){
-        console.log(scope);
-            scope.addStatus = true;
-            scope.name = "Rafal";
-            scope.age = "28";
-            scope.persons=['Tom', 'Jerry']; 
-            scope.people =[
+    .run(function($rootScope){
+      $rootScope.people =[
               {
                 name: "Sobin", 
                 age: 23
@@ -39,25 +34,108 @@
                 age: 13
               }
             ];
+    })
+    .controller('Ctrl',['$scope','$rootScope','$location', function(scope, rootScope, location){
+        console.log(scope);
+            scope.addStatus = true;
+            scope.name = "Rafal";
+            scope.age = "28";
+            scope.persons=['Tom', 'Jerry']; 
+            
           scope.add = function(){
             scope.people.push(scope.person);
             scope.person ={};
+            location.path('/');
           };
           scope.remove = function(index){
-             scope.people.splice(index, 1);
+            rootScope.people.splice(index, 1);
           };
           scope.edit = function(index){
             scope.addStatus = false;
-            scope.person = scope.people[index];
+            scope.person = rootScope.people[index];
           };
           scope.save = function(){
             scope.addStatus = true;
             scope.person = [];
           };
+        
           
           
       
     }])
-   .controller('Edit',['$scope', function(scope){
-
+   .controller('Edit',['$scope','$routeParams' function(scope, routeParams){
+      scope.id =routeParams.id
     }]);
+
+//Second Version
+//
+//    angular.module('myApp',['ngRoute']).config(function( $routeProvider){
+//      $routeProvider
+//      .when('/',{
+//        templateUrl: 'list.html',
+//        controller: 'Ctrl'
+//      })
+//      .when('/edit/:id',{
+//        templateUrl: 'edit.html',
+//        controller: 'Edit'
+//      })
+//    });
+//
+//     angular.module('myApp',['ngRoute']).run(function($rootScope){
+//      $rootScope.people =[
+//              {
+//                name: "Sobin", 
+//                age: 23
+//              },
+//              {
+//                name: "Darshan",
+//                age: 74
+//              },
+//              {
+//                name: "Prath",
+//                age: 20
+//              },
+//              {
+//                name: "Surya",
+//                age: 28
+//              },
+//              {
+//                name: "Hari",
+//                age: 13
+//              }
+//            ];
+//    });
+//
+//     angular.module('myApp',['ngRoute']).controller('Ctrl',['$scope','$rootScope','$location', function(scope, rootScope){
+//        console.log(scope);
+//            scope.addStatus = true;
+//            scope.name = "Rafal";
+//            scope.age = "28";
+//            scope.persons=['Tom', 'Jerry']; 
+//            
+//          scope.add = function(){
+//            scope.people.push(scope.person);
+//            scope.person ={};
+//          };
+//          scope.remove = function(index){
+//            rootScope.people.splice(index, 1);
+//          };
+//          scope.edit = function(index){
+//            scope.addStatus = false;
+//            scope.person = rootScope.people[index];
+//          };
+//          scope.save = function(){
+//            scope.addStatus = true;
+//            scope.person = [];
+//          };
+//          
+//          
+//      
+//    }]);
+//
+//    angular.module('myApp',['ngRoute']).controller('Edit',['$scope','$routeParams' function(scope, routeParams){
+//      scope.id =routeParams.id
+//    }]);
+
+
+angular.module('myApp',['ngRoute']).config().run().controller().controller();
